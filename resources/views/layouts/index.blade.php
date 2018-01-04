21e09aba86677f7b6762fd9e6481d7d28ef83112
Jordan Center <BigJman1234567@yahoo.com>
2018-01-04 10:16:28 -0600

    updating index page to use bootstrap grid

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,33 +1,35 @@
 @extends('layouts.master')
 
 @section('content')
-  <div class="container header">
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-10 header">
         <h3>Welcome to Atkins' Seed Service!</h3>
       </div>
 
-      <div class="container">
+      <div class="row col-10">
         <p>
           Having been with Pioneer for over 40 years, Dale Atkins knows the seed business!  In 1998, he and his wife Sheryle built a large facility on U.S. Route 24 to house their growing business.  Their son Dean also joined the business to help in its expansion.
         </p>
       </div>
 
-      <div class="image">
-        <img src="assets/AtkinsSeedStaff.png" style="width:100%; max-width: 600px; max-height: 400px">
+      <div class="row justify-content-center image">
+        <img src="assets/AtkinsSeedStaff.png" style="width:100%; height: 100%">
       </div>
 
-      <div class="container">
+      <div class="row col-10 justify-content-center">
         <p>
           Besides offering the traditional seed corn, soybeans and wheat to area farmers, the Atkins' offer agronomy services, and seed treatment.  Joining Precision Planting in 1997 has provided a way for them to help local farmers take advantage of cutting-edge technology when planting.
         </p>
       </div>
 
-      <div class="container">
+      <div class="row col-10 justify-content-center">
         <p>
           Finally, being integrally involved in the area planting, it is natural for us to offer these same farmers another alternative for their crop insurance needs.  The company is now affiliated with Rural Community Insurance Services which offers a full line of services for area farmers.
         </p>
       </div>
 
-      <div class="container contact">
+      <div class="col-10 contact">
         <div class="card-deck">
           <div class="card">
             <div class="card-body">
@@ -39,4 +41,6 @@
           </div>
         </div>
       </div>
+  </div>
+</div>
 @endsection
