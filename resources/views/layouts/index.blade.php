23fa2f0d40f59461457648c97c223f5a269d2e79
Jordan Center <BigJman1234567@yahoo.com>
2018-01-04 12:13:09 -0600

    index page elements aligned

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,28 +3,28 @@
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
-      <div class="col-10 header">
-        <h3>Welcome to Atkins' Seed Service!</h3>
+      <div class="justify-content-center">
+        <h3 style="padding-top: 100px; font-size: 30px">Welcome to Atkins' Seed Service!</h3>
       </div>
 
-      <div class="row col-10">
-        <p>
+      <div class="row col-12 justify-content-center">
+        <p class="text-width">
           Having been with Pioneer for over 40 years, Dale Atkins knows the seed business!  In 1998, he and his wife Sheryle built a large facility on U.S. Route 24 to house their growing business.  Their son Dean also joined the business to help in its expansion.
         </p>
       </div>
 
-      <div class="row justify-content-center image">
+      <div class="justify-content-center">
         <img src="assets/AtkinsSeedStaff.png" style="width:100%; height: 100%;">
       </div>
 
-      <div class="row col-10 justify-content-center">
-        <p>
+      <div class="row col-12 justify-content-center">
+        <p class="text-width">
           Besides offering the traditional seed corn, soybeans and wheat to area farmers, the Atkins' offer agronomy services, and seed treatment.  Joining Precision Planting in 1997 has provided a way for them to help local farmers take advantage of cutting-edge technology when planting.
         </p>
       </div>
 
-      <div class="row col-10 justify-content-center">
-        <p>
+      <div class="row col-12 justify-content-center">
+        <p class="text-width">
           Finally, being integrally involved in the area planting, it is natural for us to offer these same farmers another alternative for their crop insurance needs.  The company is now affiliated with Rural Community Insurance Services which offers a full line of services for area farmers.
         </p>
       </div>
